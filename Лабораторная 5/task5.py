7a66d0e33d488eaad2afcac6784e814558dd7324
cytylaydog <115294330+cytylaydog@users.noreply.github.com>
2022-11-13 22:37:30 +0300

    Delete Лабораторная 5 directory

--- Лабораторная 5/task5.py
+++
@@ -1,7 +0,0 @@
-import random
-import string
-def get_random_password(size=8):
-    pandom = string.ascii_letters + string.ascii_lowercase + string.digits
-    parol = " ".join(random.sample(pandom,size))
-    return parol
-print(get_random_password())
